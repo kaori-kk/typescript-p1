568f8cee981c577ca78fc7736067d3d5427d55f2
kaori kohama <kkaori131415@gmail.com>
2020-07-02 14:46:51 +0900

    selecting elements

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor(){
+    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+  }
+
+  private attach(){
+    
+  }
+}
