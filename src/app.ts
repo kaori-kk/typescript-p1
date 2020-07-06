3741d607f9c169bdb2eebb18b9ac437043d495b6
kaori kohama <kkaori131415@gmail.com>
2020-07-06 10:04:38 +0900

    rendering project list

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,34 @@ function autobind(
   return adjDescriptor;
 }
 
+//project class list
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished"){
+    this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent(){
+    const listId = `${this.type}-projects-list`
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement("beforeend", this.element)
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -96,9 +124,9 @@ class ProjectInput {
       max: 5 
     }
     if(
-      validate(titleValidation) &&
-      validate(descriptionValidation) &&
-      validate(peopleValidation)
+      !validate(titleValidation) ||
+      !validate(descriptionValidation) ||
+      !validate(peopleValidation)
     ){
       alert("Invalid input. Please fill all the form.")
       return;
@@ -127,3 +155,5 @@ class ProjectInput {
 }
 
 const prjName = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
